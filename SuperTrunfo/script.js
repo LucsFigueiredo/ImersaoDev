2af007b8bb185688ad9ddc01c8bd87735b78f9f0
LucsFigueiredo <96503815+LucsFigueiredo@users.noreply.github.com>
2022-09-20 14:08:31 -0300

    Add files via upload

---
+++ SuperTrunfo/script.js
@@ -0,0 +1,342 @@
+var carta1 = {
+  nome: "Blastoise",
+  imagem:
+    "https://i.pinimg.com/originals/30/bc/3b/30bc3b60fa9fc9588390f45c4af1680c.png",
+  atributos: {
+    ataque: 6,
+    defesa: 7,
+    magia: 5
+  }
+};
+
+var carta2 = {
+  nome: "Saitama",
+  imagem:
+    "https://criticalhits.com.br/wp-content/uploads/2022/04/blob_7gmc.jpg",
+  atributos: {
+    ataque: 10,
+    defesa: 8,
+    magia: 1
+  }
+};
+
+var carta3 = {
+  nome: "Ainz Ooal Gown",
+  imagem: "https://i.ytimg.com/vi/N2McDipJfmk/maxresdefault.jpg",
+  atributos: {
+    ataque: 2,
+    defesa: 8,
+    magia: 10
+  }
+};
+
+var carta4 = {
+  nome: "Mob",
+  imagem:
+    "https://i.pinimg.com/564x/36/ce/8f/36ce8f51ae1e0b3659bddcf3f404cafc.jpg",
+  atributos: {
+    ataque: 9,
+    defesa: 9,
+    magia: 10
+  }
+};
+
+var carta5 = {
+  nome: "Zoro",
+  imagem:
+    "https://i.pinimg.com/564x/eb/44/d5/eb44d5aad0159f0e5d766f0cd5d7a7f1.jpg",
+  atributos: {
+    ataque: 8,
+    defesa: 6,
+    magia: 1
+  }
+};
+
+var carta6 = {
+  nome: "Ichigo",
+  imagem:
+    "https://i.pinimg.com/564x/33/ae/a3/33aea3ebfd35b5bef1a88237fc083693.jpg",
+  atributos: {
+    ataque: 7,
+    defesa: 5,
+    magia: 1
+  }
+};
+
+var carta7 = {
+  nome: "Luffy",
+  imagem:
+    "https://i.pinimg.com/564x/1e/11/05/1e110596e63371ce01d964dc944f7576.jpg",
+  atributos: {
+    ataque: 7,
+    defesa: 7,
+    magia: 1
+  }
+};
+
+var carta8 = {
+  nome: "Hinata Shoyo",
+  imagem:
+    "https://i.pinimg.com/564x/e2/f9/cf/e2f9cfe1272cde058432f942420af45f.jpg",
+  atributos: {
+    ataque: 7,
+    defesa: 1,
+    magia: 0
+  }
+};
+
+var carta9 = {
+  nome: "Bogi",
+  imagem:
+    "https://i.pinimg.com/564x/11/95/ef/1195ef250e8c04b91e4eeb107a475f90.jpg",
+  atributos: {
+    ataque: 8,
+    defesa: 3,
+    magia: 1
+  }
+};
+
+var carta10 = {
+  nome: "Harry Potter",
+  imagem:
+    "https://i.pinimg.com/564x/7c/df/46/7cdf46e7664d8c87e92abe6f45d46c6d.jpg",
+  atributos: {
+    ataque: 5,
+    defesa: 5,
+    magia: 9
+  }
+};
+
+var carta11 = {
+  nome: "Voldemort",
+  imagem:
+    "https://i.pinimg.com/564x/9a/2d/47/9a2d4700df8a9e657d65b3967f430fa2.jpg",
+  atributos: {
+    ataque: 6,
+    defesa: 5,
+    magia: 8
+  }
+};
+
+var carta12 = {
+  nome: "Daenerys",
+  imagem:
+    "https://i.pinimg.com/originals/6e/74/29/6e7429d298e2c8611c6c0cadd87dfe76.gif",
+  atributos: {
+    ataque: 2,
+    defesa: 2,
+    magia: 5
+  }
+};
+
+var carta13 = {
+  nome: "Rimuru",
+  imagem:
+    "https://external-preview.redd.it/QEs64VerKjmzEjCFwPPZFfsNKbYh7j5NNNHvZPK-Vys.jpg?auto=webp&s=5ba7e2a56ceda050b304b4bc011bb97d9f045325",
+  atributos: {
+    ataque: 9,
+    defesa: 9,
+    magia: 10
+  }
+};
+
+var carta14 = {
+  nome: "Eren",
+  imagem:
+    "https://i.pinimg.com/564x/e6/8e/12/e68e12e13c69640928b375c78e178510.jpg",
+  atributos: {
+    ataque: 6,
+    defesa: 3,
+    magia: 5
+  }
+};
+
+var carta15 = {
+  nome: "Midoriya",
+  imagem:
+    "https://i.pinimg.com/564x/46/d7/4f/46d74f4ccdd87da1cee47cb4fdbdaf21.jpg",
+  atributos: {
+    ataque: 5,
+    defesa: 4,
+    magia: 5
+  }
+};
+
+var carta16 = {
+  nome: "Naruto",
+  imagem:
+    "https://i.pinimg.com/564x/c4/27/b9/c427b9a35897b4791c82e29dcc0b6484.jpg",
+  atributos: {
+    ataque: 5,
+    defesa: 5,
+    magia: 7
+  }
+};
+
+var carta17 = {
+  nome: "Goku",
+  imagem:
+"https://elcomercio.pe/resizer/HzL9aW6FQGXkKAGw69IRxDL3aSI=/1200x675/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/6Y2EDIISGFGVFANEVDCR5LCG34.jpg",
+  atributos: {
+    ataque: 7,
+    defesa: 5,
+    magia: 7
+  }
+};
+
+var carta18 = {
+  nome: "Gon",
+  imagem:
+    "https://i.pinimg.com/564x/9f/8e/e3/9f8ee3aa76c44e864b86e58c4c0c02b1.jpg",
+  atributos: {
+    ataque: 7,
+    defesa: 6,
+    magia: 5
+  }
+};
+
+var carta19 = {
+  nome: "Itadori",
+  imagem:
+    "https://i.pinimg.com/564x/94/7d/c4/947dc45eab53963047be1cfa539de1b9.jpg",
+  atributos: {
+    ataque: 7,
+    defesa: 6,
+    magia: 5
+  }
+};
+
+var carta20 = {
+  nome: "Sasuke",
+  imagem:
+    "https://i.pinimg.com/564x/7d/72/9d/7d729df93261119cd7da92e933a4a34c.jpg",
+  atributos: {
+    ataque: 5,
+    defesa: 5,
+    magia: 7
+  }
+};
+
+var carta21 = {
+  nome: "Tom",
+  imagem:
+    "https://i.pinimg.com/564x/54/4d/28/544d28f895914b1e31c9fadc79d0cd5f.jpg",
+  atributos: {
+    ataque: 9,
+    defesa: 9,
+    magia: 9
+  }
+};
+
+var carta22 = {
+  nome: "Chamas",
+  imagem:
+    "https://i.pinimg.com/564x/4e/71/6a/4e716ac3563a9455807d7886ce309ee8.jpg",
+  atributos: {
+    ataque: 5,
+    defesa: 4,
+    magia: 5
+  }
+};
+
+var carta23 = {
+  nome: "Salsicha",
+  imagem:
+    "https://i.pinimg.com/564x/52/14/f0/5214f00a8a6c78a9f523e48885e0725b.jpg",
+  atributos: {
+    ataque: 10,
+    defesa: 10,
+    magia: 10
+  }
+};
+
+var cartas = [
+  carta1,
+  carta2,
+  carta3,
+  carta4,
+  carta5,
+  carta6,
+  carta7,
+  carta8,
+  carta9,
+  carta10,
+  carta11,
+  carta12,
+  carta13,
+  carta14,
+  carta15,
+  carta16,
+  carta17,
+  carta18,
+  carta19,
+  carta20,
+  carta21,
+  carta22,
+  carta23
+];
+var cartaMaquina;
+var cartaJogador;
+
+function sortearCarta() {
+  var indexCartaMaquina = parseInt(Math.random() * 24);
+  cartaMaquina = cartas[indexCartaMaquina];
+
+  var indexCartaJogador = parseInt(Math.random() * 24);
+  while (indexCartaMaquina == indexCartaJogador) {
+    var indexCartaJogador = parseInt(Math.random() * 24);
+  }
+  cartaJogador = cartas[indexCartaJogador];
+
+  document.getElementById("btnSortear").disabled = true;
+  document.getElementById("btnJogar").disabled = false;
+
+  exibirCarta(cartaJogador, "carta-jogador");
+}
+
+function exibirCarta(carta, id) {
+  var cartaExibida = document.getElementById(id);
+  cartaExibida.style.backgroundImage = `url(${carta.imagem})`;
+  var moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent-ajustado.png" style=" width: inherit; height: inherit; position: absolute;">';
+  var tagHTML = "<div id='opcoes' class='carta-status'>";
+  
+  var opcoesTexto = "";
+  for (var atributo in cartaJogador.atributos) {
+    opcoesTexto +=
+      "<br><input type='radio' name='atributo' value='" + atributo + "'/>" + atributo + " " + carta.atributos[atributo];
+  }
+  var nome = `<p class="carta-subtitle">${carta.nome}</p>`
+  
+  cartaExibida.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>";
+  
+}
+
+function obtemAtributoSelecionado() {
+  var radioAtributos = document.getElementsByName("atributo");
+
+  for (var i = 0; i < radioAtributos.length; i++) {
+    if (radioAtributos[i].checked) {
+      return radioAtributos[i].value;
+    }
+  }
+}
+
+function jogar() {
+  var atributoSelecionado = obtemAtributoSelecionado();
+  var elementoResultado = document.getElementById("resultado");
+  var valorCartaJogador = cartaJogador.atributos[atributoSelecionado];
+  var valorCartaMaquina = cartaMaquina.atributos[atributoSelecionado];
+
+  exibirCarta(cartaMaquina, "carta-maquina");
+
+  if (valorCartaJogador > valorCartaMaquina) {
+    htmlResultado = "<p class='resultado-final'>Você venceu!, o atributo da carta escolhido, é maior que o da máquina.</p>";
+  } else if (valorCartaMaquina > valorCartaJogador) {
+    htmlResultado = "<p class='resultado-final'>Você perdeu, o atributo da carta escolhido, é menor que o da máquina.</p>";
+  } else {
+    htmlResultado = "<p class='resultado-final'>Empatou</p>";
+  }
+  elementoResultado.innerHTML = htmlResultado;
+  
+  document.getElementById("btnJogar").disabled = true;
+}
